1857bf2b3ea7e523443d8feb90b12db8711f91fa
Qing Lan <qingla@amazon.com>
2021-07-06 15:47:35 -0700

    provide plot function

--- analyzer/wave.py
+++ analyzer/wave.py
@@ -1,6 +1,6 @@
 from spleeter.audio.adapter import AudioAdapter
 from spleeter.separator import Separator
-
+import matplotlib.pyplot as plt
 from analyzer import utils
 
 
@@ -10,6 +10,13 @@ def get_waveform(path: str):
     waveform, _ = audio_loader.load(path, sample_rate=sample_rate)
     return waveform, audio_loader
 
+def visualize_wave(waveform):
+    plt.figure()
+    plt.plot(waveform)
+    plt.xlabel("Sample Index")
+    plt.ylabel("Amplitude")
+    plt.title("Waveform of Audio")
+    plt.show()
 
 if __name__ == '__main__':
     file_url = "https://github.com/deezer/spleeter/raw/master/audio_example.mp3"
@@ -17,7 +24,8 @@ if __name__ == '__main__':
     utils.downloader(file_url, download_path)
     separator = Separator('spleeter:2stems')
     waveform, loader = get_waveform(download_path)
-    print(waveform.shape)
+    # under the shape of (amplitude, number of channels)
+    visualize_wave(waveform)
     predictions = separator.separate(waveform)
     print(predictions["vocals"].shape)
 
